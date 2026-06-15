d0d45e5e15ff213df58df834f4f5ed3ff5ea268a
Brandon Neth <brandon.neth@hpe.com>
2026-06-15 14:59:47 -0500

    workflow that does the sweep and generates the plots
    Signed-off-by: Brandon Neth <brandon.neth@hpe.com>

--- workflows/scaling/e4s-cl-preexec.sh
+++ workflows/scaling/e4s-cl-preexec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Restore LD_LIBRARY_PATH for OTF2 libraries inside the container
 export LD_LIBRARY_PATH="/opt/otf2/lib:${LD_LIBRARY_PATH}"
-export CHPL_RT_MAX_HEAP_SIZE=70%
+export CHPL_RT_MAX_HEAP_SIZE=50%
